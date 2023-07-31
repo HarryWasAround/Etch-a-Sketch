881093065c6cf035d80d2690c1e9d9ae6bf6f18f
HarryWasAround <haralampustsipouroglou@gmail.com>
2023-07-31 23:02:35 +0300

    Add extra style to page.

---
+++ script.js
@@ -0,0 +1,39 @@
+let createDivs = (num) => {
+    num =  prompt('Number of squares per side of the grid:')
+    num = parseInt(num, 10);
+    
+    if (num > 100) {
+        alert("Your imput exceeds the limit of 100 squares");
+        return;
+    }
+
+    const container = document.getElementById('container');
+
+    container.style.cssText = `grid-template-columns: repeat(${num}, auto); grid-template-rows: repeat(${num}, auto);`
+    let count;
+
+    for (let i = 0; i < num * num; i++) {
+        count = container.childElementCount;
+        const item = document.createElement('div')
+        item.classList.add('item');
+        item.classList.add(`item${count}`);
+        
+        container.appendChild(item);
+    }
+}
+
+createDivs();
+
+let changeToBlack = () => {
+    const items = document.querySelectorAll('div');
+    items.forEach((item) => {
+        item.addEventListener("mouseover", (e) => {
+            itemClass = `${e.target.className}`;
+            itemClass = itemClass.substr(5);
+            let square = document.querySelector(`.${itemClass}`);
+            square.classList.add('hovered');
+        });
+    });
+}
+
+changeToBlack();
